5a9f273849feee4a510d9982202a811921275c40
Manasa M <manasamahadev016@gmail.com>
2026-02-09 15:54:27 +0530

    commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>hello</h1>
-    </>
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+    </h1>
   )
 }
 
