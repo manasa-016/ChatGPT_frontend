40a8b8377639e2bda449ada1ba29d96ea84bb8e8
Manasa M <manasamahadev016@gmail.com>
2026-02-11 11:52:58 +0530

    commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,23 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
-import Home from './pages/Home'
+import Home from './pages/home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <Router>
-      <nav style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}>
-        <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
-        <Link to="/about" style={{ color: 'white', marginRight: '1rem' }}>About</Link>
-        <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
-      </nav>
-      
+      <Header/>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer/>
     </Router>
   )
 }
---
+++ my-project/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <nav style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}>
+        <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
+        <Link to="/about" style={{ color: 'white', marginRight: '1rem' }}>About</Link>
+        <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
